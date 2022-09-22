109c36861aa4e049c9410fb0f1d492ebc3ce0e96
Levi <levis2k18@gmail.com>
2022-09-22 07:34:31 -0700

    Task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,18 +9,6 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while (j < n && *(src + j))
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
-	if (j < n)
-		*(dest + i) = *(src + j);
+	strncpy(dest, src, n);
 	return (dest);
 }
