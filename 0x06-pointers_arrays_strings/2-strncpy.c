7de552691bf9b98992818380b14df0256742565f
Levi <levis2k18@gmail.com>
2022-09-22 07:40:49 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strncat - appends src to the dest string
+ * _strncpy - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  * @n: largest number of bytes to append
- * Return: address of dest
+ * Return: string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	strncpy(dest, src, n);
 	return (dest);
